43ad49a26c39e58ac208af7f0236e8f09ed7f3d5
Sreenivas Makam <smakam@yahoo.com>
2016-01-18 19:44:16 +0530

    count 16

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ redis = Redis(host=os.environ.get('REDIS_HOST', 'redis'), port=6379)
 
 @app.route('/')
 def hello():
-    visits=redis.incr('counter', 15)
+    visits=redis.incr('counter', 16)
     html = "<h3>Hello {name}!</h3>" \
            "<b>Hostname:</b> {hostname}<br/>" \
            "<b>Visits:</b> {visits}<br/>"
